7cb3dd2cf4a7d97151df97cfcfb98c288c76417d
Darin Gregg <daringregg801@gmail.com>
2023-10-03 08:52:59 -0600

    partway through example

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
 
+  const [people, setPeople] = useState([{"name":"Yoda"}]);
+
   return (
     <div>
-
+      <h1>Star Wars People</h1>
+      <ul>
+        {people.map(() => {
+          return <li>{person.name}</li>
+        })}
+      </ul>
     </div>
   )
 
