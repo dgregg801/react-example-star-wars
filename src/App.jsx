407d4f0f4a0b8bfc5b8793ab4b61c45eabc86bf7
Darin Gregg <daringregg801@gmail.com>
2023-10-05 13:06:33 -0600

    React router example finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,37 @@
-import { useState } from "react";
-import "./App.css";
+import { BrowserRouter, Route, Routes, NavLink } from "react-router-dom";
+import CharactersPage from "./pages/charactersPage";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import './App.css';
 
-function App() {
+//Install react -router -dom
+//Wrap the application in BrowserRouter
+//Set up the routes and route
+//each rout wants a path and a an element
+//set up user navigation using NavLink elements
 
-  const [people, setPeople] = useState([{"name":"Yoda"}]);
 
+export default function App() {
   return (
-    <div>
-      <h1>Star Wars People</h1>
+    <BrowserRouter>
+    <nav>
       <ul>
-        {people.map(() => {
-          return <li>{person.name}</li>
-        })}
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">Contact</NavLink>
+        </li>
+        <li>
+          <NavLink to="/characters">Characters</NavLink>
+        </li>
       </ul>
-    </div>
+    </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/characters" element={<CharactersPage />} />
+      </Routes>
+    </BrowserRouter>
   )
-
-  
 }
-
-export default App;
